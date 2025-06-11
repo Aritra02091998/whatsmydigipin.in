7abf4b8f935c87810afdd2061d70d94d47db44c3
Aritra Dutta <aritra.duttas18@gmail.com>
2025-06-11 17:45:00 +0530

    Articles added

---
+++ scripts/blog-page-tailwind-config.js
@@ -0,0 +1,21 @@
+tailwind.config = {
+    darkMode: 'class',
+    theme: {
+        extend: {
+            colors: {
+                primary: {
+                    light: '#2563eb',
+                    dark: '#3b82f6'
+                },
+                secondary: {
+                    light: '#0284c7',
+                    dark: '#0ea5e9'
+                },
+                accent: {
+                    light: '#f97316',
+                    dark: '#ea580c'
+                }
+            }
+        }
+    }
+}
